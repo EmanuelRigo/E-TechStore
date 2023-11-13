7699b8c9369987c4805097cfb23df23b476402f7
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-11-13 10:16:40 -0300

    agregando css a confirmPay // pc-escritorio

--- src/components/ConfirmPay.jsx
+++ src/components/ConfirmPay.jsx
@@ -63,6 +63,67 @@ function ConfirmPay() {
         <button onClick={handleContinue}>confirmar</button>
       </div>
       <Link to={linkHabilitado ? "/showDetails" : "#"}>continuar</Link>
+      <div class="container mt-4 d-flex justify-content-center main">
+        <div class="card">
+          <div class="d-flex justify-content-between px-3 pt-4">
+            <span class="pay">Pay amount</span>
+            <div class="amount">
+              <div class="inner">
+                <span class="dollar">$</span>
+                <span class="total">999</span>
+              </div>
+            </div>
+          </div>
+          <div class="px-3 pt-3">
+            <label for="card number" class="d-flex justify-content-between">
+              <span class="labeltxt">CARD NUMBER</span>
+              <img src="download (4).png" width="25" class="image" />
+            </label>
+            <input
+              type="number"
+              name="number"
+              class="form-control inputtxt"
+              placeholder="8881 2545 2545 2245"
+            />
+          </div>
+          <div class="d-flex justify-content-between px-3 pt-4">
+            <div>
+              <label for="date" class="exptxt">
+                Expiry
+              </label>
+              <input
+                type="number"
+                name="number"
+                class="form-control expiry"
+                placeholder="MM / YY"
+              />
+            </div>
+            <div>
+              <label for="cvv" class="cvvtxt">
+                CVV /CVC
+              </label>
+              <input
+                type="number"
+                name="number"
+                class="form-control cvv"
+                placeholder="123"
+              />
+            </div>
+          </div>
+          <div class="d-flex align-items-center justify-content-between px-3 py-4">
+            <div>
+              <button type="button" class="btn cancel">
+                Cancel
+              </button>
+            </div>
+            <div>
+              <button type="button" class="btn payment">
+                Make Payment
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
